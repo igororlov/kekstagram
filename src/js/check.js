6599156c404a14b7021e12651132197b9c984683
Igor Orlov <igororlov92@mail.ru>
2016-10-09 12:58:47 +0200

    Fixed Travis' comments.

--- src/js/check.js
+++ src/js/check.js
@@ -2,28 +2,30 @@
 
 function getMessage(a, b) {
   var result;
-  if (typeof a === "boolean") {
+  if (typeof a === 'boolean') {
     if (a) {
-      result = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+      result = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     } else {
-      result = "Переданное GIF-изображение не анимировано";
+      result = 'Переданное GIF-изображение не анимировано';
     }
-  } else if (typeof a === "number") {
-    result = "Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
+  } else if (typeof a === 'number') {
+    result = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
   } else if (Array.isArray(a)) {
     if (!Array.isArray(b)) {
-      result = "Количество красных точек во всех строчках изображения: " + getArraySum(a);
+      result = 'Количество красных точек во всех строчках изображения: ' + getArraySum(a);
     } else {
-      result = "Общая площадь артефактов сжатия: " + getDotProduct(a, b) + " пикселей";
+      result = 'Общая площадь артефактов сжатия: ' + getDotProduct(a, b) + ' пикселей';
     }
   } else {
-    result = "Переданы некорректные данные";
+    result = 'Переданы некорректные данные';
   }
   return result;
 }
 
-function getArraySum(a) {
-  return a.reduce(function(a, b) { return a + b; }, 0);
+function getArraySum(arr) {
+  return arr.reduce(function(a, b) {
+    return a + b;
+  }, 0);
 }
 
 function getDotProduct(a, b) {
